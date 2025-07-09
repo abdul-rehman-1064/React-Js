db8a2d58f8c1c84d6b2dda7c5a12e35249d26dfe
Abdul Rehman <arehman.ansari1064@gmail.com>
2025-07-09 14:28:13 +0500

    React Js Basic & Advance

--- 12appWrite_Blog/src/components/Logo.jsx
+++ 12appWrite_Blog/src/components/Logo.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
+import logo from '../assets/logo.jpeg'
 
-function Logo({width = '100px'}) {
+function Logo({width = '70px'}) {
   return (
-    <div>Logo</div>
+   <img src={logo} width={50} className='rounded-full' alt="" />
   )
 }
 
